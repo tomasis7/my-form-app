b82fa881fdb523be5ece6df4ccd2a7b3dee6e245
tomasis7 <tomasis7@gmail.com>
2025-02-24 23:20:05 +0100

    feat: add new user entries to db.json; implement user registration functionality in userService

--- src/api/userService.ts
+++ src/api/userService.ts
@@ -3,16 +3,16 @@ interface UserData {
     email: string;
 }
 
-// export const registerUser = async (userData: UserData) => {
-//     const response = await fetch("http://localhost:3000/users", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify(userData),
-//     });
-//     return response.json();
-// };
+export const registerUser = async (userData: UserData) => {
+    const response = await fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+    });
+    return response.json();
+};
 
 export const fetchUsers = async () => {
     const response = await fetch("http://localhost:3000/users");
