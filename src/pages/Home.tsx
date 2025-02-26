cf8314ad1cb30e8fa039c442baad221adb72ef71
tomasis7 <tomasis7@gmail.com>
2025-02-26 22:18:02 +0100

    feat: add links to the React Form App and its GitHub repository in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,12 @@ const Home = () => {
         This application demonstrates form handling and CRUD operations using
         React and the React Router library.
       </p>
+      <a href="https://react-form-router-v7-improved.vercel.app/">Go to the React Form App with Router V7</a>
+      <br />
+      <br />
+      <a href="https://github.com/tomasis7/react-form-routerV7-improved/">Go to the Github Repo of that site above</a>
+      <br />
+      <br />
     </div>
   );
 };
